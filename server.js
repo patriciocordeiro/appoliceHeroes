4d8f835ddd83b8851259dd8521482775e963382a
patriciocordeiro <paponcio6@gmail.com>
2017-09-01 23:16:47 -0300

    adicionando os primeiros heros ao iniciar

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 //var session = require('express-session');
 var path = require('path');
+var Heroes = require('./server/heroes/heroes.model');
+var heroesFakeData = require('./server/testUtils/heroesFakeData');
 
+console.log(heroesFakeData);
 
 var heroesApi = require('./server/heroes/heroes.api');
 
@@ -31,6 +34,24 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
     console.log('connected to database');
+
+
+    /*We use bellow just to check if there is no heroes in db an then we add some (For test only)*/
+    Heroes.find({}, function (error, heroes) {
+        if (error) {
+            console.log(error);
+        } else if (!heroes || heroes.length < 1) {
+            //Insert heroes
+            console.log('no heoroes, creating new ones');
+            heroesFakeData.forEach(function (hero) {
+                var newHeroes = new Heroes(hero);
+                newHeroes.save(function (err, data) {
+                    if (err) console.log(err);
+                    console.log(data);
+                })
+            })
+        }
+    });
 
 });
 
